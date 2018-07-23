34567029cffc88186fe69992d38c352d2b05c241
Harran J. Williams <hayron24@gmail.com>
2018-07-23 15:55:18 -0400

    Modified code to nave a custom list view of earthquake objects.  This includes a pojo, arrayadapter, and layout xml file.

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,28 @@
+package com.example.android.quakereport;
+
+import java.util.Date;
+
+public class Earthquake {
+
+    private double magnitude;
+    private String location;
+    private Date date;
+
+    public Earthquake(double magnitude, String location, Date date) {
+        this.magnitude = magnitude;
+        this.location = location;
+        this.date = date;
+    }
+
+    public double getMagnitude() {
+        return magnitude;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+}
--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -21,6 +21,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
@@ -32,20 +33,20 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_activity);
 
         // Create a fake list of earthquake locations.
-        ArrayList<String> earthquakes = new ArrayList<>();
-        earthquakes.add("San Francisco");
-        earthquakes.add("London");
-        earthquakes.add("Tokyo");
-        earthquakes.add("Mexico City");
-        earthquakes.add("Moscow");
-        earthquakes.add("Rio de Janeiro");
-        earthquakes.add("Paris");
+        ArrayList<Earthquake> earthquakes = new ArrayList<Earthquake>();
+        earthquakes.add(new Earthquake(7.2, "San Francisco", new Date()));
+        earthquakes.add(new Earthquake(3.5, "London", new Date()));
+        earthquakes.add(new Earthquake(5.4, "Tokyo", new Date()));
+        earthquakes.add(new Earthquake(2.2, "Mexico City", new Date()));
+        earthquakes.add(new Earthquake(6.9, "Moscow", new Date()));
+        earthquakes.add(new Earthquake(1.8, "Rio de Janeiro", new Date()));
+        earthquakes.add(new Earthquake(5.1, "Paris", new Date()));
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         // Create a new {@link ArrayAdapter} of earthquakes
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+        ArrayAdapter<Earthquake> adapter = new ArrayAdapter<Earthquake>(
                 this, android.R.layout.simple_list_item_1, earthquakes);
 
         // Set the adapter on the {@link ListView}
