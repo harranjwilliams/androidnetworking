3b78c3b9c7542a588e42b495293b73c8db457e3f
harranjwilliams <hayron24@gmail.com>
2018-08-16 08:20:52 -0400

    Started replacing list view with recycler view

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,6 +17,9 @@ package com.example.android.quakereport;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -27,6 +30,9 @@ public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
 
+    private RecyclerView mRecyclerView;
+    private EarthquakeAdapter mEarthquakeAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,16 +40,27 @@ public class EarthquakeActivity extends AppCompatActivity {
 
         // Create a fake list of earthquake locations.
         ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+        Log.i(LOG_TAG, "Earthquakes list size = " + earthquakes.size());
 
         // Find a reference to the {@link ListView} in the layout
-        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+        // Find a reference to the {@link RecyclerView} in the layout instead
+        mRecyclerView = findViewById(R.id.recycler_view);
 
-        // Create a new {@link ArrayAdapter} of earthquakes
-        ArrayAdapter<Earthquake> adapter = new ArrayAdapter<Earthquake>(
-                this, android.R.layout.simple_list_item_1, earthquakes);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
 
-        // Set the adapter on the {@link ListView}
-        // so the list can be populated in the user interface
-        earthquakeListView.setAdapter(adapter);
+        mRecyclerView.setHasFixedSize(true);
+
+        mEarthquakeAdapter = new EarthquakeAdapter(this, earthquakes);
+
+        mRecyclerView.setAdapter(mEarthquakeAdapter);
+
+//        // Create a new {@link ArrayAdapter} of earthquakes
+//        ArrayAdapter<Earthquake> adapter = new ArrayAdapter<Earthquake>(
+//                this, android.R.layout.list_item, earthquakes);
+//
+//        // Set the adapter on the {@link ListView}
+//        // so the list can be populated in the user interface
+//        earthquakeListView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,8 +1,12 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,50 +20,117 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
+public class EarthquakeAdapter extends RecyclerView.Adapter<EarthquakeAdapter.EarthquakeViewHolder> {
+
+    public static final String LOG_TAG = EarthquakeAdapter.class.getName();
 
     private DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+
+    private List<Earthquake> mEarthquakes;
 
     public EarthquakeAdapter(Activity context, List<Earthquake> earthquakes) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // Because this is a custom adapter for multiple TextViews, the adapter is not
         // going to use this second argument, so it can be any value. Here, we used 0.
-        super(context, 0, earthquakes);
+        //super(context, 0, earthquakes);
+
+        mEarthquakes = earthquakes;
+        Log.i(LOG_TAG, "Earthquakes list size = " + mEarthquakes.size());
+
+    }
+
+    public class EarthquakeViewHolder extends RecyclerView.ViewHolder{
+
+        public final TextView mMagnitudeView;
+        public final TextView mLocationView;
+        public final TextView mDateView;
+
+        public EarthquakeViewHolder(View itemView) {
+            super(itemView);
+
+            mMagnitudeView = itemView.findViewById(R.id.magnitude);
+            mLocationView = itemView.findViewById(R.id.location);
+            mDateView = itemView.findViewById(R.id.date);
+        }
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View listItemView = convertView;
+    public EarthquakeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        boolean attachToRoot = false;
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
 
-        // Check if the existing view is being reused, otherwise inflate the view
-        if (listItemView == null) {
-            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
-        }
+        View view = layoutInflater.inflate(R.layout.list_item, parent, attachToRoot);
+        EarthquakeViewHolder viewHolder = new EarthquakeViewHolder(view);
 
-        // Get the {@link Earthquake} object located at this position in the list
-        Earthquake earthquake = getItem(position);
-
-        // Find the TextView in the list_item.xml layout with the ID magnitude
-        TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magnitude);
-        // Get the magnitude from the current Earthquake object and
-        // set this double on the magnitude TextView
-        magnitudeTextView.setText(Double.toString(earthquake.getMagnitude()));
-
-        // Find the TextView in the list_item.xml layout with the ID location
-        TextView locationTextView = (TextView) listItemView.findViewById(R.id.location);
-        // Get the location from the current Earthquake object and
-        // set this text on the location TextView
-        locationTextView.setText(earthquake.getLocation());
-
-        // Find the TextView in the list_item.xml layout with the ID date
-        TextView dateTextView = (TextView) listItemView.findViewById(R.id.date);
-        // Get the date from the current Earthquake object and
-        // set this date on the date TextView
-        dateTextView.setText(dateFormat.format(earthquake.getDate()));
-
-        // Return the whole list item layou
-        return listItemView;
+        Log.i(LOG_TAG, "View holder created");
+        return viewHolder;
     }
+
+    @Override
+    public void onBindViewHolder(@NonNull EarthquakeViewHolder holder, int position) {
+
+        Earthquake earthquake = mEarthquakes.get(position);
+
+        String magnitude = Double.toString(earthquake.getMagnitude());
+        String location = earthquake.getLocation();
+        String date = dateFormat.format(new Date(earthquake.getDate()));
+
+        Log.i(LOG_TAG, "Position = " + position + " Magnitude = " + magnitude + " Location = " + location + " Date = " + date);
+
+        holder.mMagnitudeView.setText(magnitude);
+        holder.mLocationView.setText(location);
+        holder.mDateView.setText(date);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mEarthquakes == null) {
+            return 0;
+        }
+        return mEarthquakes.size();
+    }
+
+    public void setEarthQuakeData(List<Earthquake> earthquakes) {
+        mEarthquakes = earthquakes;
+        notifyDataSetChanged();
+    }
+
+//    @NonNull
+//    @Override
+//    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//        View listItemView = convertView;
+//
+//        // Check if the existing view is being reused, otherwise inflate the view
+//        if (listItemView == null) {
+//            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+//        }
+//
+//        // Get the {@link Earthquake} object located at this position in the list
+//        Earthquake earthquake = getItem(position);
+//
+//        // Find the TextView in the list_item.xml layout with the ID magnitude
+//        TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magnitude);
+//        // Get the magnitude from the current Earthquake object and
+//        // set this double on the magnitude TextView
+//        magnitudeTextView.setText(Double.toString(earthquake.getMagnitude()));
+//
+//        // Find the TextView in the list_item.xml layout with the ID location
+//        TextView locationTextView = (TextView) listItemView.findViewById(R.id.location);
+//        // Get the location from the current Earthquake object and
+//        // set this text on the location TextView
+//        locationTextView.setText(earthquake.getLocation());
+//
+//        // Find the TextView in the list_item.xml layout with the ID date
+//        TextView dateTextView = (TextView) listItemView.findViewById(R.id.date);
+//        // Get the date from the current Earthquake object and
+//        // set this date on the date TextView
+//        long date = earthquake.getDate();
+//        dateTextView.setText(dateFormat.format(new Date(date)));
+//
+//        // Return the whole list item layou
+//        return listItemView;
+//    }
 }
